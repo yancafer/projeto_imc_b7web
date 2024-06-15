915a95ac379f1fba315049ddaf717fa7816d3cff
Yanca <yancafernandes2@gmail.com>
2024-06-14 22:31:12 -0500

    Adição de calculo imc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
 
 const App = () => {
+
+  const [heighField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+
+  const handleCalculateButton = () => {
+    if(heighField && weightField){
+      const imc = weightField / (heighField * heighField);
+      alert(`Seu IMC é: ${imc.toFixed(2)}`);
+    }else {
+      alert('Preencha os campos!');
+    }
+  }
+
   return (
     <div className={styles.main}>
       <header>
@@ -16,6 +30,23 @@ const App = () => {
             pela Organização Mundial de Saúde para calcular o peso ideal de
             cada pessoa.
           </p>
+
+          <input
+            type='number'
+            placeholder='Digite a sua altura. Ex: 1.5 (em metros)'
+            value={heighField > 0 ? heighField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+
+          <input
+            type='number'
+            placeholder='Digite o seu peso. Ex: 75 (em quilos)'
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+
         </div>
         <div className={styles.rightSide}>
           ...
