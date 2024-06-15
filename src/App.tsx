e5cff7176dd3320596f45caa3532badeb0e37a22
Yanca <yancafernandes2@gmail.com>
2024-06-14 20:27:29 -0500

    Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC</h1>
+          <p>IMC é a sigla para índice de Massa Corpórea, parâmetro adotado
+            pela Organização Mundial de Saúde para calcular o peso ideal de
+            cada pessoa.
+          </p>
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
